21996a5ab5fa7612f9a8ac3fb4e7fa5352870c49
ArjunMalik2468 <76555468+ArjunMalik2468@users.noreply.github.com>
2021-04-04 11:55:28 +0530

    Add files via upload

---
+++ Sand.js
@@ -0,0 +1,22 @@
+ // additional goal 1 sand class
+class Sand {
+    constructor(x, y,radius) {
+      var options = {
+        'density':1,
+        'friction': 5,
+        'restitution':1.3
+      }
+      this.body = Bodies.circle(x, y,radius, options);
+      this.radius = radius;
+
+      World.add(world, this.body);
+    }
+    display(){
+     
+        ellipseMode(RADIUS);
+  
+        fill("yellow");
+  
+        circle(this.body.position.x,this.body.position.y,this.radius)
+    }
+  }
